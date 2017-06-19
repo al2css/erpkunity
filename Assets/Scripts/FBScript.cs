d76949bf6dbef1c2b2272c5b893d6548362d81ac
Alex Dulgheru <alecsdulgheru@gmail.com>
2017-06-19 16:26:14 +0300

    wip login to erpk route

--- Assets/Scripts/FBScript.cs
+++ Assets/Scripts/FBScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using Facebook.Unity;
 using Facebook.MiniJSON;
 
@@ -12,11 +13,14 @@ public class FBScript : MonoBehaviour {
     public GameObject Username;
     public GameObject Userpic;
 
+    WWWHelper reqManager;
     TabsGroupScript views;
 
     void Awake()
     {
-        if(!FB.IsInitialized)
+        reqManager = WWWHelper.wwwHelper;
+        
+        if (!FB.IsInitialized)
         {
             FB.Init(InitCallback, OnHideUnity);
         } else
@@ -63,15 +67,19 @@ public class FBScript : MonoBehaviour {
 
     void AuthCallback(IResult result)
     {
-        if(result.Error != null)
+        if (result.Error != null)
         {
             Debug.LogWarning(result.Error);
         } else
         {
+            var objs = Json.Serialize(result.ResultDictionary);
+            Debug.LogWarning("### AuthCallback JSON Object: " + objs);
             
             if (FB.IsLoggedIn)
             {
                 Debug.Log("AuthCallback - Fb is logged in");
+
+                LoginToErpk(result);
             }
             else
             {
@@ -80,6 +88,22 @@ public class FBScript : MonoBehaviour {
 
             ShowIfLogged(FB.IsLoggedIn);
         }
+    }
+
+    void LoginToErpk(IResult result)
+    {
+        var objs = Json.Serialize(result.ResultDictionary);
+        Debug.LogWarning("@@@ LoginToErpk JSON Object: " + objs);
+
+        var loginUrl = "https://www.erepublik.com/en/main/mobile-facebook-login";
+        Dictionary<string, string> formData = new Dictionary<string, string>();
+
+        formData.Add("accessToken", result.ResultDictionary["access_token"].ToString());
+        formData.Add("facebookId", result.ResultDictionary["user_id"].ToString());
+        formData.Add("expiresAt", result.ResultDictionary["expiration_timestamp"].ToString());
+        formData.Add("developer", "Alex");
+
+        reqManager.Post(loginUrl, formData);
     }
 
     void ShowIfLogged(bool isLoggedIn)
@@ -92,8 +116,6 @@ public class FBScript : MonoBehaviour {
             FB.API("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
             FB.API("/me/picture?type=square&height=64&width=64", HttpMethod.GET, DisplayUserPic);
 
-            FB.API("/debug_token?input_token=" + FB.ClientToken + "&access_token=" + FB.ClientToken, HttpMethod.GET, AccessTokenCallback);
-            
             TabsGroupScript.tabsGroupScript.ActivateFirstView();
         } else
         {
@@ -101,13 +123,7 @@ public class FBScript : MonoBehaviour {
             DialogLoggedOut.SetActive(true);
         }
     }
-
-    void AccessTokenCallback(IResult result)
-    {
-        var objs = Json.Serialize(result.ResultDictionary);
-        Debug.LogWarning("### AccessTokenCallback JSON Object: " + objs);
-    }
-
+    
     void DisplayUsername(IResult result)
     {
         var objs = Json.Serialize(result.ResultDictionary);
--- Assets/Scripts/WWWHelper.cs
+++ Assets/Scripts/WWWHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using Facebook.MiniJSON;
 
 public class WWWHelper : MonoBehaviour {
 
@@ -19,17 +21,36 @@ public class WWWHelper : MonoBehaviour {
         StartCoroutine(WaitForRequest(www));
     }
 
+    public void Post(string url, Dictionary<string, string> formData)
+    {
+        Debug.LogWarning("---- POST url: " + url);
+        Debug.LogWarning("---- POST formData: " + Json.Serialize(formData));
+
+        WWWForm form = new WWWForm();
+
+        foreach (var item in formData)
+        {
+            form.AddField(item.Key, item.Value);
+        }
+
+        Debug.LogWarning("---- POST FORM PARAMS: " + form.data);
+
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www));
+    }
+
     IEnumerator WaitForRequest(WWW www)
     {
         yield return www;
 
         if (www.error == null)
         {
-            Debug.Log("WWW OK: " + www.text);
+            Debug.Log("~~~~~ WWW OK: " + www.text);
         }
         else
         {
-            Debug.Log("WWW Error: " + www.error);
+            Debug.Log("~~~~~ WWW Error: " + www.error);
         }
     }
 }
