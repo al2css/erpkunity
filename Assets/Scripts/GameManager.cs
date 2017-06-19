e48c48409cbc81daf87ed16de1bd1edce94e982a
Alex Dulgheru <alecsdulgheru@gmail.com>
2017-06-19 18:27:40 +0300

    wip login to erpk route - cookies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,6 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        Debug.Log("GameManager Start");	
 	}
 }
